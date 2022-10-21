6b21af8f8b234d98a07a8f9d182570da1136f5b6
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2022-10-21 18:48:10 +0530

    Get attribute set attribute

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // console.log(document.getElementsByTagName("li"))
 // console.log(document.getElementsByClassName("list"))
 
-console.log(document.getElementById("heading"))
+// console.log(document.getElementById("heading"))
+// console.log(document.querySelectorAll("li"))
+
+// var head=document.getElementById("heading")
+// console.log(head.classList);
+// head.classList.toggle("head")
+// console.log(head.classList);
+
+
+// // Get attributes set attributes
+
+// var attributes=document.querySelector("h1");
+// atclassname=attributes.getAttribute("class")
+// console.log(atclassname);
+// attributes.setAttribute("class" ,"changed")
+// atclassname=attributes.getAttribute("class")
+// console.log(atclassname);  output:headingattribute changed index.js:20
+
+
